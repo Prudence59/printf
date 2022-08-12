69e4534d12df9407fa3ec376b9154901a9fefafc
Prudence59 <pokeynwankwo@gmail.com>
2022-08-12 05:31:16 -0700

    b spec

--- main.h
+++ main.h
@@ -19,5 +19,6 @@ int _putchar(char c);
 int print_c(va_list c);
 int print_s(va_list s);
 int print_i(va_list i);                           int print_d(va_list d);
+int print_b(va_list b);
 
 #endif  /* _MAIN_H */
